ddbd3cd35828c81087084d1b1ae00d41ec4c6b2d
AlexashinVladimir <96177016+AlexashinVladimir@users.noreply.github.com>
2025-09-09 17:26:16 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,7 +1,3 @@
-"""
-states.py — FSM состояния для добавления и редактирования категорий и курсов.
-"""
-
 from aiogram.fsm.state import StatesGroup, State
 
 
@@ -14,6 +10,7 @@ class AddCourse(StatesGroup):
     waiting_for_title = State()
     waiting_for_description = State()
     waiting_for_price = State()
+    waiting_for_link = State()
 
 
 class EditCourse(StatesGroup):
