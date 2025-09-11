c3e3db12a8dac39af9c8220b26c046a03dab2199
AlexashinVladimir <96177016+AlexashinVladimir@users.noreply.github.com>
2025-09-11 17:37:48 +0300

    Update states.py

--- states.py
+++ states.py
@@ -4,15 +4,15 @@ from aiogram.fsm.state import StatesGroup, State
 class AddCategory(StatesGroup):
     waiting_for_title = State()
 
+class EditCategory(StatesGroup):
+    waiting_for_new_title = State()
+
 class AddCourse(StatesGroup):
     choosing_category = State()
     waiting_for_title = State()
     waiting_for_description = State()
     waiting_for_price = State()
     waiting_for_link = State()
-
-class EditCategory(StatesGroup):
-    waiting_for_new_title = State()
 
 class EditCourse(StatesGroup):
     waiting_for_field_choice = State()
